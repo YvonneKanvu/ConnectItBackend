1989b37b1866805b5c3bc9ea3821ca9b4c55b18f
Yvonne Kanvu <yvkanvu@gmail.com>
2024-05-10 17:09:58 +0200

    make user email unique

---
+++ prisma/migrations/20240509181356_email_unique/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[email]` on the table `Utilisateur` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[telephone]` on the table `Utilisateur` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- CreateIndex
+CREATE UNIQUE INDEX "Utilisateur_email_key" ON "Utilisateur"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Utilisateur_telephone_key" ON "Utilisateur"("telephone");
